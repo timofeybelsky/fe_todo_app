f08466dce696285341cab4a174e50434d2a6cb6c
User Name <artur.it2914@gmail.com>
2020-01-24 18:12:43 +0200

    Alter: sequelize config

--- server/src/db/config/config.js
+++ server/src/db/config/config.js
@@ -1,15 +1,15 @@
+require( 'dotenv/config' );
+
 module.exports = {
   development: {
-    'username': 'arthur',
-    'password': 'admin',
-    'database': 'fe_todo_db',
-    'host': '127.0.0.1',
-    'dialect': 'postgres'
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    migrationStorage: 'json',
+    seederStorage: 'json'
   },
-  test: {
-
-  },
-  production: {
-
-  }
+  test: {},
+  production: {}
 };

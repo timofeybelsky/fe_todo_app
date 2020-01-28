d2b8b0f6eb161410356f5f93e7bfa246a2e9b4c4
User Name <artur.it2914@gmail.com>
2020-01-28 18:38:37 +0200

    Create: router and userRouter

--- server/src/index.js
+++ server/src/index.js
@@ -1,28 +1,8 @@
-import express  from 'express';
-import { User } from './db/models';
+import express from 'express';
+import router  from './routes';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use( express.json() );
-
-app.get( '/', (req, res) => res.send( 'Hello World!' ) );
-
-app.post( '/user', async (req, res, next) => {
-  try {
-
-    const createdUser = await User.create( req.body );
-
-    return res.send( createdUser );
-
-  } catch (e) {
-    next( e );
-  }
-} );
-
-app.use( (err, req, res) => {
-
-  res.status( 500 ).send( 'Something broke!' );
-
-} );
-
+app.use( router );
 app.listen( PORT, () => console.log( `Example app listening on port ${PORT}!` ) );

95c0d8f54ffc9096e43f27731c2734f9b7edb1de
User Name <artur.it2914@gmail.com>
2020-01-30 20:12:04 +0200

    Add: task router

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,12 +2,14 @@ import express                from 'express';
 import AppErrors              from '../utils/applicationErrors';
 import userRouter             from './user.router.js';
 import checkUserAuthorization from '../middlewares/authorization/checkUserAuthorization.js';
+import taskRouter             from './task.router.js';
 
 const router = express.Router();
 
 router.use( checkUserAuthorization );
 
 router.use( '/user', userRouter );
+router.use( '/task', taskRouter );
 
 router.use( '/*', function (req, res, next) {
   next( new AppErrors.NotFoundError() );

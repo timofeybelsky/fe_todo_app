f99a09d16cb446c32b7aaa6ff59ee94dbb8e7ec1
User Name <artur.it2914@gmail.com>
2020-01-30 19:49:22 +0200

    Refactor: validationErrorHandler.js again

--- server/src/middlewares/errorHandlers/validationErrorHandler.js
+++ server/src/middlewares/errorHandlers/validationErrorHandler.js
@@ -3,19 +3,9 @@ import { ValidationError } from '@hapi/joi';
 export default function (err, req, res, next) {
   if (err instanceof ValidationError) {
     const { details: [errorObj] } = err;
-
-    let errorMessage = '';
-
-    switch (errorObj.type) {
-      case 'string.pattern.base':
-        errorMessage = `\"${errorObj.context.label}\" with value \"${errorObj.context.value}\" fails to match the required pattern`;
-        break;
-      default:
-        errorMessage = errorObj.message;
-        break;
-    }
-
-    return res.status( 400 ).send( errorMessage );
+    return res.status( 400 ).send( errorObj.type === 'string.pattern.base'
+                                   ? `\"${errorObj.context.label}\" with value \"${errorObj.context.value}\" fails to match the required pattern`
+                                   : errorObj.message );
   } else {
     next( err );
   }

c851e888b65ce668a4f5d5d1b24d462f7bf7d963
User Name <artur.it2914@gmail.com>
2020-01-31 19:33:51 +0200

    Create: task router

--- server/src/routes/task.router.js
+++ server/src/routes/task.router.js
@@ -1,10 +1,23 @@
-import express from 'express';
+import express             from 'express';
+import createValidationMW  from '../middlewares/validation/createValidationMW.js';
+import schemas             from '../utils/validation';
+import * as TaskController from '../controllers/task.controller.js';
 
 const taskRouter = express.Router();
 
-taskRouter.post( '/', );
-taskRouter.get( '/', );
-taskRouter.patch( '/', );
-taskRouter.delete( '/', );
+taskRouter.post( '/',
+                 createValidationMW( schemas.taskSchema )(),
+                 TaskController.createTask
+);
+taskRouter.get( '/:taskId',
+                TaskController.getTask
+);
+taskRouter.patch( '/:taskId',
+                  createValidationMW( schemas.taskSchema )( false ),
+                  TaskController.updateTask
+);
+taskRouter.delete( '/:taskId',
+                   TaskController.deleteTask
+);
 
 export default taskRouter;

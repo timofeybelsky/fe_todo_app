63fff11a9b8484fcd2922fc32f86bb99a6337f69
User Name <artur.it2914@gmail.com>
2020-01-31 20:09:20 +0200

    Add: checkPermissionsMW to routes

--- server/src/routes/task.router.js
+++ server/src/routes/task.router.js
@@ -2,21 +2,29 @@ import express            from 'express';
 import createValidationMW from '../middlewares/validation/createValidationMW.js';
 import schemas            from '../utils/validation';
 import TaskController     from '../controllers/task.controller.js';
+import checkPermissions   from '../middlewares/permission/checkPermissions.js';
+import { ACTION, ENTITY } from '../constants';
 
 const taskRouter = express.Router();
 
+const checkTaskPermissions = checkPermissions( ENTITY.TASK );
+
 taskRouter.post( '/',
+                 checkTaskPermissions( ACTION.CREATE ),
                  createValidationMW( schemas.taskSchema )(),
-                 TaskController.createTask
+                 TaskController.createTask,
 );
 taskRouter.get( '/:taskId',
+                checkTaskPermissions( ACTION.READ ),
                 TaskController.getTask
 );
 taskRouter.patch( '/:taskId',
+                  checkTaskPermissions( ACTION.UPDATE ),
                   createValidationMW( schemas.taskSchema )( false ),
                   TaskController.updateTask
 );
 taskRouter.delete( '/:taskId',
+                   checkTaskPermissions( ACTION.DELETE ),
                    TaskController.deleteTask
 );
 
--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -3,19 +3,31 @@ import { createUser, deleteUserByPk, getUserByPk, updateUserByPk } from '../cont
 import createValidationMW
                                                                    from '../middlewares/validation/createValidationMW.js';
 import schemas                                                     from '../utils/validation';
+import checkPermissions                                            from '../middlewares/permission/checkPermissions.js';
+import { ACTION, ENTITY }                                          from '../constants';
 
 const userRouter = express.Router();
 
+const checkUserPermissions = checkPermissions( ENTITY.USER );
+
 userRouter.post( '/',
+                 checkUserPermissions( ACTION.CREATE ),
                  createValidationMW( schemas.userSchema )(),
                  createUser
 );
 userRouter.patch( '/:userId',
+                  checkUserPermissions( ACTION.UPDATE ),
                   createValidationMW( schemas.userSchema )( false ),
                   updateUserByPk
 );
-userRouter.get( '/:userId', getUserByPk );
+userRouter.get( '/:userId',
+                checkUserPermissions( ACTION.READ ),
+                getUserByPk
+);
 
-userRouter.delete( '/:userId', deleteUserByPk );
+userRouter.delete( '/:userId',
+                   checkUserPermissions( ACTION.DELETE ),
+                   deleteUserByPk
+);
 
 export default userRouter;

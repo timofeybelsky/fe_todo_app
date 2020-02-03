f4acdda1c01b959727ab0fed8c38ed4f18460d23
User Name <artur.it2914@gmail.com>
2020-02-03 19:25:11 +0200

    Create: m:n association between user&role models

--- server/src/models/role.js
+++ server/src/models/role.js
@@ -1,10 +1,19 @@
 'use strict';
+import { ROLE } from '../constants';
+
 module.exports = (sequelize, DataTypes) => {
-  const Role = sequelize.define('Role', {
-    name: DataTypes.ENUM
-  }, {});
-  Role.associate = function(models) {
-    // associations can be defined here
+  const Role = sequelize.define( 'Role', {
+    name: {
+      type: DataTypes.ENUM( ...Object.values( ROLE ) ),
+      allowNull: false,
+      unique: true,
+    }
+  }, {} );
+  Role.associate = function (models) {
+    Role.belongsToMany( models.User, {
+      through: 'UserRoles',
+
+    } );
   };
   return Role;
 };
